1784177174b004146c6a8b67c739f9a1d30bc24c
JSpeed8th <jspeed8th@gmail.com>
2017-12-07 23:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ternaryCheckCity (city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-    return 'Thank you so much';
+    return 'Thank you so much.';
   }
-  
+
 }
