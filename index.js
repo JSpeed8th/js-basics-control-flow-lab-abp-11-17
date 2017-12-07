40fc0de5c5c8a9a8dbf2a70c748ea8d97ecee2d7
JSpeed8th <jspeed8th@gmail.com>
2017-12-07 23:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function scuberGreetingForFeet (feet) {
 
 }
 
-functionCheckCity (city) {
+function ternaryCheckCity (city) {
   if (city === 'NYC') {
-    return 'Ok, sounds good.'
+    return 'Ok, sounds good.';
   }
 }
